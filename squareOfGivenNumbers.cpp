e5be85f2521ed2f66086162be58f04b34674e571
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-03-19 15:46:29 +0530

    Add files via upload

---
+++ squareOfGivenNumbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a number : ";
+    cin>>x;
+
+    int y = x*x;
+    cout<<"The Sqaure of the number is : " <<y;
+
+}
