596921cfb778bd169dd669c733585e4a32583358
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-03-21 23:56:08 +0530

    Add files via upload

---
+++ halfOfinteger.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the value of x :"<<endl;
+    cin>>x;  //x = 7
+    float y = (float)x; //y = 7
+    cout<<"The value is  : "<<" "<<y/2;
+}
